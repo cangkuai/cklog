cbfadb1ebd883fcf9c9fdcb8a1f71ab2a8a921a5
cangkuai <33569578+cangkuai@users.noreply.github.com>
2022-06-09 17:47:04 +0800

    support 1.19

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,89 @@
+package com.example.examplemod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("examplemod")
+public class ExampleMod
+{
+    public String languages,blocks;
+    public String pass="no";
+    public String[] whitelist=null;
+    public ArrayList<String> languagesit=new ArrayList<String>();
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ExampleMod()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the enqueueIMC method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        // Register the processIMC method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+        // Some example code to dispatch IMC to another mod
+        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+    }
+
+    private void processIMC(final InterModProcessEvent event)
+    {
+        // Some example code to receive and process InterModComms from other mods
+        LOGGER.info("Got IMC {}", event.getIMCStream().
+                map(m->m.messageSupplier().get()).
+                collect(Collectors.toList()));
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        ckreader ckreaders=new ckreader();
+        HashMap<String, Object> sitting=ckreaders.getsit();
+        languages=(String) sitting.get("language");
+        pass=(String) sitting.get("pass");
+        whitelist=(String[]) sitting.get("whitelist");
+        blocks=(String) sitting.get("blocks");
+        languagesit=ckreaders.getlanguagesit();
+    }
+    @SubscribeEvent
+    public void breakss(BlockEvent.BreakEvent event) {
+        new breakchack(event,languages,blocks,pass,whitelist,languagesit);
+    }
+    @SubscribeEvent
+    public void breakss(BlockEvent.EntityPlaceEvent event) {
+        new EntityPlace(event);
+    }
+    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+    // Event bus for receiving Registry Events)
+
+}
---
+++ src/main/java/com/example/examplemod/breakchack.java
@@ -0,0 +1,79 @@
+package com.example.examplemod;
+import net.minecraft.Util;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.event.world.BlockEvent;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+public class breakchack {
+    public breakchack(BlockEvent.BreakEvent event, String languages, String blocks, String pass, String[] whitelist, ArrayList<String> languagesit){
+        String x =String.valueOf(event.getPos().getX());
+        String y =String.valueOf(event.getPos().getY());
+        String z =String.valueOf(event.getPos().getZ());
+        String players=event.getPlayer().getName().getString();
+        String curDir = System.getProperty("user.dir");
+        String times=String.valueOf( System.currentTimeMillis()/1000);
+        String names=event.getState().getBlock().getName().getString();
+        String ls;
+        String menss=x+","+y+","+z;
+        Integer len=menss.length();
+        File file = new File(curDir+"/mods/ckapp/data.txt");
+        if (!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if(whitelist!=null)
+        {
+            for(String i:whitelist){
+                if (i.equals(players)){
+                    pass="ok";
+                }
+            }
+        }
+        if (names.equals(blocks)&&pass.equals("ok")){
+            event.getPlayer().sendSystemMessage(Component.translatable(languagesit.get(0)));
+            long startTime = System.currentTimeMillis();
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                while ((ls= br.readLine()) != null) {
+                    String ls2=ls.substring(0,len);
+                    if (ls2.equals(menss)){
+                        String types="";
+                        String[] ls1=ls.split(",");
+                        if (ls1[6].equals("0")){
+                            types=languagesit.get(1);
+                        }else {
+                            types=languagesit.get(2);
+                        }
+                        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        String dateString = formatter.format(new Date(Long.parseLong(ls1[4]+"000")));
+                        event.getPlayer().sendSystemMessage(Component.translatable(ls1[3] + " " + dateString + " " + ls1[5] + " " + types));
+                        if(whitelist!=null){
+                            pass="no";
+                        }
+                    }
+                }
+            }catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            long endTime = System.currentTimeMillis();
+            event.getPlayer().sendSystemMessage(Component.translatable(languagesit.get(3) + (endTime - startTime) + " ms"));
+        }
+        try {
+            FileWriter writer = new FileWriter(file,true);
+            writer.write(x+","+y+","+z+","+players+","+times+","+names+",0\n");
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
